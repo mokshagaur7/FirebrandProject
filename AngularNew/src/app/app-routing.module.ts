c7e3a0263c576c8ed68ce533728e829d754bef06
GheorgheMilea <polironald@yahoo.com>
2023-10-02 15:00:48 +0100

    Works

--- AngularNew/src/app/app-routing.module.ts
+++ AngularNew/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SettingsComponent } from './settings/settings.component';
+import { ListComponent } from './list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'', children: [{path:'',component:HomeComponent},{path:'list',component:ListComponent}]},
+{path:'login',component:LoginComponent},
+{path:'profile',component:ProfileComponent},
+{path:'settings',component:SettingsComponent},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AngularNew/src/app/app.module.ts
+++ AngularNew/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SettingsComponent } from './settings/settings.component';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProfileComponent,
+    SettingsComponent,
+    HomeComponent,
+    ListComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
