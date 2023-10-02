4ffb957d27f8988333d59edc8851639c1d94a77c
GheorgheMilea <polironald@yahoo.com>
2023-10-02 12:02:09 +0100

    Navbar and Login page

--- Angular/AngularApp/src/app/app-routing.module.ts
+++ Angular/AngularApp/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SettingsComponent } from './settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'list',component:ListComponent},
+  {path:'login',component:LoginComponent},
+  {path:'profile',component:ProfileComponent},
+  {path:'settings',component:SettingsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
