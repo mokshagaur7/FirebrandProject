84341d69ca679f742ede69f8359174a20d1bae63
andreadhelpra <andrea.dhelpra19@gmail.com>
2023-10-02 15:48:44 +0100

    pull data for IBM

--- AngularNew/src/app/list/list.component.ts
+++ AngularNew/src/app/list/list.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent {
+export class ListComponent implements OnInit{
+  private url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo';
 
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchData();
+  }
+
+  fetchData(): void {
+    this.httpClient.get<any>(this.url, {headers: {'User-Agent': 'my-app'}}).subscribe(
+      data => {
+        console.log(data);
+      },
+      error => {
+        console.log('Error:', error);
+      }
+    );
+  }
 }
