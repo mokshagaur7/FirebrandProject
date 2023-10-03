c3a0ef0a2b342a7465be92fba07e66878962bc63
Moksha <mokshagaur7@gmail.com>
2023-10-03 16:37:42 +0100

    Load More button for lists

--- AngularNew/src/app/list/list.component.ts
+++ AngularNew/src/app/list/list.component.ts
@@ -11,6 +11,7 @@ export class ListComponent implements OnInit {
   //listComponent: Location[] = [];
   symbols: any[] = [];
   filteredSymbols: any[]=[];
+  displayedItems: number = 15; // Initial number of items to display
   isLoading: boolean = true;
   
   tokenApi: string = environment.apiToken;
@@ -29,13 +30,19 @@ export class ListComponent implements OnInit {
       console.log(data);
       this.symbols = data;
       this.symbols = data.map(symbol => ({ ...symbol, selected: false }));
-      this.filteredSymbols = data;
+      this.filteredSymbols = this.symbols.slice(0, this.displayedItems); // Display the first 15 items
       //this.symbols = this.filteredSymbols;
       this.isLoading = false;
     }, error => {
       this.isLoading = false;
       console.error('Error fetching symbols:', error);
     });
+  }
+
+
+  loadMore(): void {
+    this.displayedItems += 15; // Load the next 15 items
+    this.filteredSymbols = this.symbols.slice(0, this.displayedItems);
   }
 
  
