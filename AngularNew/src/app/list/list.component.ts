2bd53a37b1e14f0ce5ef3d572b09d81ac7fa0884
Moksha <mokshagaur7@gmail.com>
2023-10-03 14:45:25 +0100

    search bar

--- AngularNew/src/app/list/list.component.ts
+++ AngularNew/src/app/list/list.component.ts
@@ -8,7 +8,9 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  //listComponent: Location[] = [];
   symbols: any[] = [];
+  filteredSymbols: any[]=[]
   tokenApi: string = environment.apiToken;
   private iexCloudAPI = `https://cloud.iexapis.com/stable/ref-data/symbols?token=${this.tokenApi}`;
 
@@ -16,14 +18,37 @@ export class ListComponent implements OnInit {
 
   ngOnInit(): void {
     this.fetchSymbols();
+
+    
   }
 
   fetchSymbols(): void {
     this.httpClient.get<any[]>(this.iexCloudAPI).subscribe(data => {
       console.log(data);
       this.symbols = data;
+      this.filteredSymbols = data;
+      //this.symbols = this.filteredSymbols;
     }, error => {
       console.error('Error fetching symbols:', error);
     });
   }
+
+ 
+
+  filterResults(text: string): void {
+    if (!text) {
+      this.filteredSymbols = this.symbols; // Reset to all symbols when the search text is empty
+      return;
+    }
+
+    const regex = new RegExp(text, 'i'); // 'i' flag for case-insensitive matching
+    this.filteredSymbols = this.symbols.filter(symbol => regex.test(symbol.symbol));
+  }
+
+  handleSearchInputChange(text: string): void {
+    if (!text) {
+      this.filteredSymbols = this.symbols; // Return the complete list when the search text is empty
+    }
+  }
+  
 }
