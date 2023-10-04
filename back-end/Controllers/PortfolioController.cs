68fe78c0c64180edd7f11d467864228e64388909
andreadhelpra <andrea.dhelpra19@gmail.com>
2023-10-04 10:59:40 +0100

    add stock ids to portfolio

--- back-end/Controllers/PortfolioController.cs
+++ back-end/Controllers/PortfolioController.cs
@@ -7,9 +7,10 @@ public class PortfolioController : ControllerBase
 {
     private static readonly List<Portfolio> portfolios = new List<Portfolio>
     {
-        new Portfolio { Id = 1, Name = "EVs" },
-        new Portfolio { Id = 2, Name = "American stocks" },
-        new Portfolio { Id = 3, Name = "European stocks"}
+        new Portfolio { Id = 1, Name = "EVs", StockIds = new int[] {1} },
+        new Portfolio { Id = 2, Name = "American stocks", StockIds = new int[] {2} },
+        new Portfolio { Id = 3, Name = "European stocks", StockIds = new int[] {1, 2}},
+        new Portfolio { Id = 4, Name = "Chinese stocks", StockIds = new int[] {1, 2, 3}}
     };
 
     [HttpGet]
@@ -22,5 +23,6 @@ public class PortfolioController : ControllerBase
     {
         public int? Id { get; set; }
         public string? Name { get; set; }
+        public int[]? StockIds { get; set; }
     }
 }
