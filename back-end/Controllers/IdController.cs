b92e0a6ffb46e2c51a43db9242890385bf3d1270
GheorgheMilea <polironald@yahoo.com>
2023-10-05 14:54:59 +0100

    Navigation

--- back-end/Controllers/IdController.cs
+++ back-end/Controllers/IdController.cs
@@ -16,6 +16,24 @@ public class IdController : ControllerBase
         return Ok(new { Message = "UserdId caught."+UseraId});
     }
 
+    public static void UserProfile(int id){
+            string connectionStr = "server=127.0.0.1;database=StockPortfolioDB;user=root;password=MyPassword1234";
+            using(MySqlConnection sqlconnection = new MySqlConnection(connectionStr)){
+                try
+                {
+                    sqlconnection.Open();
+                    string sqlQuery = $"Update Ids Set Id = '{email}' WHERE username = '{username}'" ;
+                    using (MySqlCommand cmd = new MySqlCommand(sqlQuery,sqlconnection)){ 
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception e){
+                    Console.WriteLine($"Error: {e.Message}");
+                }
+            }
+        }
+
+
     [HttpPost]
     public IActionResult UserId([FromBody] UsersId userId)
     {
