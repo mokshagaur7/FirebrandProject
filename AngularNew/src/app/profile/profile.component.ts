04f5928c1905233c51621a67b19c77e21f481c6b
Moksha <mokshagaur7@gmail.com>
2023-10-05 12:08:14 +0100

    Profile page POSTING to database

--- AngularNew/src/app/profile/profile.component.ts
+++ AngularNew/src/app/profile/profile.component.ts
@@ -9,23 +9,56 @@ import { Router } from '@angular/router';
 })
 export class ProfileComponent implements OnInit {
   users: any[] = [];
+  editUsername: boolean = false;
+  editEmail: boolean = false;
+  editedUsername: string = ''; // Variable to store edited username
+  editedEmail: string = ''; // Variable to store edited email
 
-  apiUrl : string = 'http://localhost:5040/api/login';
+  apiUrl: string = 'http://localhost:5040/api/login';
   token: undefined;
-  constructor(private router: Router,private http:HttpClient) {      
-    this.token = undefined;  
+
+  constructor(private router: Router, private http: HttpClient) {
+    this.token = undefined;
   }
 
   ngOnInit(): void {
     this.http.get(this.apiUrl).subscribe(
-      (data:any) => {
+      (data: any) => {
         this.users = data;
-        console.log(this.users);
+        this.editedUsername = this.users[0]?.username; // Initialize with current username
+        this.editedEmail = this.users[0]?.email; // Initialize with current email
       })
   }
 
-  
+  toggleEdit(field: string): void {
+    if (field === 'username') {
+      this.editUsername = !this.editUsername;
+    } else if (field === 'email') {
+      this.editEmail = !this.editEmail;
+    }
+  }
 
-  
+  saveChanges(): void {
+    // Update user details in the API
+    const updatedUserData = {
+      username: this.editedUsername,
+      email: this.editedEmail
+      // Add other properties as needed
+    };
 
+    this.http.post(this.apiUrl, updatedUserData).subscribe(
+      (response: any) => {
+        console.log('User details updated:', response);
+        // Optionally, you can update the local user data as well
+        this.users[0].username = this.editedUsername;
+        this.users[0].email = this.editedEmail;
+        // Disable editing after successful update
+        this.editUsername = false;
+        this.editEmail = false;
+      },
+      (error: any) => {
+        console.error('Error updating user details:', error);
+      }
+    );
+  }
 }
